3876e3cab5898bce0b10c360207ae14a5745ba13
Inga Bemman (R0otChiXor) <R0otChiXor@gmail.com>
2018-04-23 22:32:55 -0600

    added opium mvvm to the project

---
+++ Wallet/Model/AddressBookEntry.cs
@@ -0,0 +1,39 @@
+﻿using Opium.MVVM.Framework.Model;
+
+namespace Wallet.Model
+{
+    public class AddressBookEntry:BaseNotify
+    {
+        private string _name;
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                _name = value;
+                RaisePropertyChanged(() => Address);
+            }
+        }
+
+        private string _address;
+        public string Address
+        {
+            get => _address;
+            set
+            {
+                _address = value;
+                RaisePropertyChanged(() => Address);
+            }
+           
+            
+        }
+
+        public AddressBookEntry(string name, string address)
+        {
+            Name = name;
+            Address = address;
+        }
+
+        
+    }
+}
--- Wallet/Properties/AssemblyInfo.cs
+++ Wallet/Properties/AssemblyInfo.cs
@@ -7,13 +7,13 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Wallet")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Opium Bitcoind Wallet")]
+[assembly: AssemblyDescription("WPF Wallet for bitcoin type coin")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Wallet")]
+[assembly: AssemblyCompany("R0otChixor")]
+[assembly: AssemblyProduct("Opium Wallet - Bitcoin Private")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTrademark("Binary Omen")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
