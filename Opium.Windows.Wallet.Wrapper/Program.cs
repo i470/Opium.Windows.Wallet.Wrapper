0db13c5798f46d4fe7f1225223f3f5afd053c049
Inga Bemman (R0otChiXor) <R0otChiXor@gmail.com>
2018-04-23 20:46:10 -0600

    Add project files.

---
+++ Opium.Windows.Wallet.Wrapper/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opium.Windows.Wallet.Wrapper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
